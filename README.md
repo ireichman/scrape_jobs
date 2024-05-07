3e5d6bfaf37a9352fcdd264c15eeeca6600402a5
ireichman <isreli@ireichman.com>
2024-05-06 23:27:29 -0600

    Intial commit. Main file has a few comments with next steps. Readme has a description.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Job Scraper.
+
+## Description:
+Given a list of websites and a list of keywords that could be in a the job title/ description.
+The program will make a list or relevant job postings on those websites and email a list of 
+relevant jobs with a link for applying to them.
---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+Scrape websites for jobs postings containing keywords.
+"""
+
+# Set up selenium or BeautifulSoup.
+
+# Read list of websites to scrape.
+
+# Read list of keywords to look for.
+
+# Scrape a website for careers/ jobs/ corporate/ etc.
+
+# Iterate through found listings
+
+# Look for next type buttons.
+
+# If next button found, Scrape next page for listings and report until no more listings available.
