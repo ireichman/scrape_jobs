320371b81555542d6c2097ddcb7c2be6f3b68f93
ireichman <isreli@ireichman.com>
2024-05-12 22:11:53 -0600

    Added funciton for reading keywords/ websites from a file. moving html handling to html class in html_handling.py.

---
+++ html_handling.py
@@ -0,0 +1,29 @@
+"""
+
+"""
+from bs4 import BeautifulSoup as bs
+import requests
+from loguru import logger
+
+
+class html:
+
+    def __init__(self, html_raw):
+        self.html = bs(markup=html_raw.content, features="html.parser")
+
+    def search_elements(self, element_type: str, string_text: str):
+        """
+
+        :param element_type:
+        :param string_text:
+        :return:
+        """
+        pass
+
+    def keywords_search(self, keywords):
+        """
+
+        :param keywords:
+        :return:
+        """
+        pass
--- main.py
+++ main.py
@@ -3,12 +3,47 @@ Scrape websites for jobs postings containing keywords.
 """
 from bs4 import BeautifulSoup as bs
 import requests
+from loguru import logger
 
 # Set up selenium or BeautifulSoup.
 url = "https://realpython.github.io/fake-jobs/"
 page = requests.get(url=url)
+soup = bs(markup=page.content, features="html.parser")
 
-soup = bs(page.content, "html.parser")
+
+def list_from_file(file: str):
+    """
+    Read a text file and break text in to a list of words by new line.
+    Designed for 'keywords' and 'websites' but can take any file name or path.
+    :param file: Specify 'keywords' or 'websites'. Otherwise, give it the full file name and path if not in the base folder.
+    :return: List of strings.
+    """
+    logger.info(f"Reading {file}")
+    if file == "keywords":
+        file = "keywords.txt"
+    elif file == "websites":
+        file = "websites.txt"
+    else:
+        file = file
+    try:
+        with open(file=file, mode='r') as file_words:
+            words_raw = file_words.read()
+    except Exception as error:
+        logger.error(f"Attempting to read {file} returned an error:\n{error}")
+    words_list = words_raw.splitlines()
+    words = [word.lower() for word in words_list]
+    logger.info(f"Reading {file} returned the following list:\n{words}")
+    return words
+
+
+def search_elements(element_type: str, string_text: str):
+    """
+    Find elements of specific type and
+    :param element_type:
+    :param string_text:
+    :return:
+    """
+    ""
 
 
 def keywords_search(html_string: str, keywords: list):
@@ -43,11 +78,12 @@ for job in relevant_jobs_full_card:
     job_title = job.find("h2", class_="title")
     job_company = job.find("h3", class_="company")
     job_location = job.find("p", class_="location")
-    job_link = job.
+    job_links = job.find_all(name="a", string=lambda text: "apply" in text.lower())
+    # job_link = job.
     print(f"Title: {job_title.text} "
           f"\nCompany: {job_company.text} "
           f"\nLocation: {job_location.text.strip()} "
-          f"\nJob Posting: {}")
+          f"\nApplication link: {job_links[0]['href']}")
 
 # Look for next type buttons.
 
