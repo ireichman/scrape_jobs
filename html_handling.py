60ede048a5f59fc279847ffdc8042c60174ac022
ireichman <isreli@ireichman.com>
2024-05-13 22:24:14 -0600

    Wrote the docstring for search_elements in HTML class.

--- html_handling.py
+++ html_handling.py
@@ -6,21 +6,24 @@ import requests
 from loguru import logger
 
 
-class html:
+class HTML:
 
     def __init__(self, html_raw):
         self.html = bs(markup=html_raw.content, features="html.parser")
 
-    def search_elements(self, element_type: str, string_text: str):
+    def search_elements(self, element_type: str, string_text: list, element_attribute: str = None):
         """
-
-        :param element_type:
-        :param string_text:
-        :return:
+        Find HTML elements who's string match any of the strings in string_text. Optionally, Specify a different HTML
+        attribute that string_text should match.
+        :param element_type: Specify what type of element to search for.
+        :param string_text: A list of strings to search for in the html 'string' attribute (unless element_attribute is
+        specified).
+        :param element_attribute: Optionally, specify an HTML attribute to search in instead of 'string'.
+        :return: ???
         """
         pass
 
-    def keywords_search(self, keywords):
+    def keywords_search(self, keywords: list):
         """
 
         :param keywords:
