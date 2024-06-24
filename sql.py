cf89ebe449c07359baf7593ed83ad18228afbbd8
ireichman <isreli@ireichman.com>
2024-06-23 23:42:35 -0600

    Created Sql class skeleton.

---
+++ sql.py
@@ -0,0 +1,19 @@
+"""
+Class for handling creation of DB, as well as writing, reading and deleting data.
+"""
+
+
+class Sql:
+
+    def __init__(self):
+        pass
+
+    def write(self):
+        pass
+
+    def read(self):
+        pass
+
+    def delete(self):
+        pass
+    
